84b9e194534722fbe5ed29f7dcaab0f436cace26
ALAMIN761740 <abdullahalamin761740@gmail.com>
2026-03-11 15:49:34 +0600

    add README file

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ let characterIndex = 0;
 updateText();
 
 function updateText() {
-    containerE1.innerHTML = `<h1 class="title">I AM A ${careers[careerIndex].slice(0, characterIndex)}</h1>`;
+    containerE1.innerHTML = `<h1 class="title">I AM ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(0, characterIndex)}</h1>`;
     characterIndex++;
     setTimeout(() => {
         if (characterIndex > careers[careerIndex].length) {
             careerIndex++;
             characterIndex = 0;
+        }
+        if (careerIndex === careers.length) {
+            careerIndex = 0;
         }
         updateText();
     }, 200);
